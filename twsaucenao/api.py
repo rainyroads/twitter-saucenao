e4938a14dc7c01fb72e58a9cb1b2669c53264311
Makoto <FujiMakoto@users.noreply.github.com>
2020-01-23 12:06:57 -0500

    Basic modules for configuration, logging and API instantiation

---
+++ twsaucenao/api.py
@@ -0,0 +1,26 @@
+import logging
+
+import tweepy
+
+from twsaucenao.config import config
+
+logger = logging.getLogger(__name__)
+
+
+def twitter_api():
+    consumer_key        = config.get('Twitter', 'consumer_key')
+    consumer_secret     = config.get('Twitter', 'consumer_secret')
+    access_token        = config.get('Twitter', 'access_token')
+    access_token_secret = config.get('Twitter', 'access_secret')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.critical("Error loading API", exc_info=True)
+        raise e
+    logger.info("Twitter API ready")
+    return api
---
+++ twsaucenao/log.py
@@ -0,0 +1,16 @@
+# Set up logging
+import logging
+
+from twsaucenao.config import config
+
+logLevel = getattr(logging, str(config.get('Logging', 'log_level', fallback='ERROR')).upper())
+logFormat = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S")
+
+log = logging.getLogger('twsaucenao')
+log.setLevel(logLevel)
+
+ch = logging.StreamHandler()
+ch.setLevel(logLevel)
+ch.setFormatter(logFormat)
+
+log.addHandler(ch)
