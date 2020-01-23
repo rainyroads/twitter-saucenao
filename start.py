acd0412b4161e3f1bbb4cd66b9c17bb445a543c6
Makoto <FujiMakoto@users.noreply.github.com>
2020-01-23 17:10:35 -0500

    Startup script and additional logging

---
+++ start.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from twsaucenao.server import TwitterSauce
+
+
+async def main():
+    twitter = TwitterSauce()
+    while True:
+        await twitter.check_mentions()
+        twitter.log.info("Waiting...")
+        await asyncio.sleep(10.0)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
