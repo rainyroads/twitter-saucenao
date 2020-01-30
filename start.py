f8bc4fd57e11234762551569986618f391d71277
Makoto <FujiMakoto@users.noreply.github.com>
2020-01-29 21:00:11 -0500

    Broad exception handling and requirements update

--- start.py
+++ start.py
@@ -1,14 +1,19 @@
 import asyncio
 
 from twsaucenao.server import TwitterSauce
+from twsaucenao.log import log
 
 
 async def main():
     twitter = TwitterSauce()
     while True:
-        await twitter.check_mentions()
-        twitter.log.info("Waiting...")
-        await asyncio.sleep(15.0)
+        try:
+            await twitter.check_mentions()
+            twitter.log.info("Waiting...")
+            await asyncio.sleep(15.0)
+        except:
+            log.exception("An unknown error occurred while checking mentions")
+            await asyncio.sleep(45.0)
 
 
 if __name__ == '__main__':
