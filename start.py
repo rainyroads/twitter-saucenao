77e0beee79d409f41887133dc20a35e32dd4adf1
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-14 19:02:11 -0400

    Custom intervals for mentions, monitored accounts and search queries. Resolves #3

--- start.py
+++ start.py
@@ -1,18 +1,31 @@
 import asyncio
 
+from twsaucenao.config import config
 from twsaucenao.server import TwitterSauce
 from twsaucenao.log import log
 
 
 async def main():
     twitter = TwitterSauce()
+
+    # Get our polling intervals
+    mentioned_interval  = float(config.get('Twitter', 'mentioned_interval', fallback=15.0))
+    monitored_interval  = float(config.get('Twitter', 'monitored_interval', fallback=60.0))
+    search_interval     = float(config.get('Twitter', 'search_interval', fallback=60.0))
+
     while True:
         try:
+            # Mentions
+            await asyncio.sleep(mentioned_interval)
             await twitter.check_mentions()
+
+            # Monitored accounts
+            await asyncio.sleep(monitored_interval)
             await twitter.check_monitored()
+
+            # Search query
+            await asyncio.sleep(search_interval)
             await twitter.check_query()
-            twitter.log.info("Waiting...")
-            await asyncio.sleep(15.0)
         except:
             log.exception("An unknown error occurred while checking mentions")
             await asyncio.sleep(45.0)
