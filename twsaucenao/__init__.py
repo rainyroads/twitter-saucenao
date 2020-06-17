b349f5e8145912c2a98b5c9e6b35d83ddad175c4
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-17 06:13:31 -0400

    Copyright metadata

--- twsaucenao/__init__.py
+++ twsaucenao/__init__.py
@@ -1,1 +1,9 @@
 SAUCENAOPLS_TWITTER_ID = 1219954473080061953
+
+__author__      = 'FujiMakoto'
+__copyright__   = 'Copyright 2020, Taiga.sh Development'
+__credits__     = ['FujiMakoto']
+__license__     = 'agpl-3.0'
+__maintainer__  = 'FujiMakoto'
+__email__       = 'makoto+github@taiga.sh'
+__status__      = 'Development'
