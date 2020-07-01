cba23392a304e947acdc60d9c427f72013e3456b
Makoto <FujiMakoto@users.noreply.github.com>
2020-06-30 23:24:05 -0400

    Major updates and refactoring. Experimental and may contain bugs. See detailed comments.
    1) Tweet parsing logic abstracted to TweetParser class
    2) Globalized the Twitter API handler (globals generally bad, but this is a bot and not meant to be used as a library)
    3) Support for providing artists with DMCA information if a user has blocked the bot added
    4) Support for displaying Manga chapters added
    5) Re-organized the reply tweet layout to prioritize the artist name / Twitter handle first and foremost

--- twsaucenao/api.py
+++ twsaucenao/api.py
@@ -7,39 +7,40 @@ from twsaucenao.config import config
 logger = logging.getLogger(__name__)
 
 
-def twitter_api():
-    consumer_key        = config.get('Twitter', 'consumer_key')
-    consumer_secret     = config.get('Twitter', 'consumer_secret')
-    access_token        = config.get('Twitter', 'access_token')
-    access_token_secret = config.get('Twitter', 'access_secret')
+def _twitter_api(key: str, secret: str, token: str, token_secret: str) -> tweepy.api.API:
+    """
+    Establish a tweepy API instance
+    Args:
+        key (str): The consumer key
+        secret (str): The consumer secret
+        token (str): The access token
+        token_secret (str): The access token secret
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    Returns:
+        tweepy.api.API
+    """
+    auth = tweepy.OAuthHandler(key, secret)
+    auth.set_access_token(token, token_secret)
+    _api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
     try:
-        api.verify_credentials()
+        _api.verify_credentials()
     except Exception as e:
         logger.critical("Error loading API", exc_info=True)
         raise e
     logger.info("Twitter API ready")
-    return api
+    return _api
 
 
-def twitter_readonly_api():
-    consumer_key        = config.get('TwitterReadOnly', 'consumer_key')
-    consumer_secret     = config.get('TwitterReadOnly', 'consumer_secret')
-    access_token        = config.get('TwitterReadOnly', 'access_token')
-    access_token_secret = config.get('TwitterReadOnly', 'access_secret')
+api = _twitter_api(config.get('Twitter', 'consumer_key'), config.get('Twitter', 'consumer_secret'),
+                   config.get('Twitter', 'access_token'), config.get('Twitter', 'access_secret'))
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        logger.critical("Error loading API", exc_info=True)
-        raise e
-    logger.info("Read-only Twitter API activated")
-    return api
+if config.has_section('TwitterReadOnly'):
+    readonly_api = _twitter_api(config.get('TwitterReadOnly', 'consumer_key'),
+                                config.get('TwitterReadOnly', 'consumer_secret'),
+                                config.get('TwitterReadOnly', 'access_token'),
+                                config.get('TwitterReadOnly', 'access_secret'))
+else:
+    readonly_api = None
+
