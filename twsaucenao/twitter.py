1976cdc28ffed6fc223b17fbc0cd2d7de8ce879c
Makoto <FujiMakoto@users.noreply.github.com>
2020-07-20 14:25:10 -0400

    WIP: Updated TweetManager class with caching implementation

--- twsaucenao/twitter.py
+++ twsaucenao/twitter.py
@@ -6,44 +6,131 @@ import tweepy
 from twsaucenao import SAUCENAOPLS_TWITTER_ID
 from twsaucenao.api import api, readonly_api
 from twsaucenao.errors import TwSauceNoMediaException
+from twsaucenao.models.database import TweetCache
 
 
-class TweetParser:
-    def __init__(self, tweet):
+class TweetManager:
+    def __init__(self):
+        """
+        Handles performing cache and API queries on tweepy objects
+        """
         self.log = logging.getLogger(__name__)
         self.my = api.me()
 
-        # The tweet to look up
-        self.tweet = tweet
-        
-        # Has the bot been blocked by this account?
-        self.blocked = False
-
-        # HTTPS Url to the tweet media
-        self._tweet_media = None
-
-    @property
-    def media(self):
-        if self._tweet_media:
-            return self._tweet_media
-
-        self._tweet_media = [m['media_url_https'] for m in self.find_closest_media()]
-        return self._tweet_media
-
-    # noinspection PyUnresolvedReferences
-    def find_closest_media(self) -> List[dict]:
+    def get_tweet(self, tweet_id: int) -> TweetCache:
         """
-        Determine whether this is a direct tweet or a reply, then parse the media accordingly
-        """
-        if self.tweet.in_reply_to_status_id:
-            return self._parse_reply(self.tweet)
+        Performs a lookup on the given tweet ID.
+        Attempts to load the tweet from database cache first, and if that fails, executes a Twitter API query.
+        Args:
+            tweet_id (int): The tweet ID
 
-        return self._parse_direct(self.tweet)
-
-    def _parse_direct(self, tweet) -> List[dict]:
+        Returns:
+            TweetCache
         """
-        Direct tweet (someone tweeting at us directly, not as a reply to another tweet)
-        Should have media attached, otherwise it's invalid and we ignore it
+        # Attempt to load from cache first
+        tweet = TweetCache.get(tweet_id)
+        if tweet:
+            return tweet
+
+        # If it's not cached yet, fetch the tweet from the API
+        blocked = False
+        try:
+            _tweet = api.get_status(tweet_id, tweet_mode='extended')
+        except tweepy.TweepError as error:
+            # If we're blocked, use readonly parsing if configured, otherwise log an error and re-throw the exception
+            if error.api_code == 136:
+                blocked = True
+                if readonly_api:
+                    self.log.warning(f"User has blocked the main account; falling back to read-only API for media parsing on tweet {tweet_id}")
+                    _tweet = readonly_api.get_status(tweet_id, tweet_mode='extended')
+                else:
+                    self.log.warning(f"Skipping tweet {tweet_id} as we have been blocked by the authors account")
+                    raise error
+            else:
+                raise error
+
+        # Cache and return
+        return TweetCache.set(_tweet, bool(self.extract_media(_tweet)), blocked=blocked)
+
+    def get_closest_media(self, tweet) -> List:
+        """
+        Find the closet media post associated with this tweet.
+        This could be this tweet itself if someone has mentioned us with an upload.
+        This could be a comment where the direct parent is the media we need to look up.
+        This could also be a comment further down a chain of comments on a parent post, in which case we will need to
+        traverse several tweets to find something to look up.
+        Args:
+            tweet: tweepy.models.Status object. Not type hinted because type hinting doesn't work with tweepy.
+
+        Raises:
+            TwSauceNoMediaException: Raised if no media entities can be found associated with this tweet
+
+        Returns:
+            Tweet
+        """
+        # If the tweet itself has media to search for, return it now
+        if self.extract_media(tweet):
+            return self.extract_media(tweet)
+
+        if self._is_bot_reply(tweet):
+            self.log.info('Skipping a tweet that is a comment on a post by the bot account')
+            raise TwSauceNoMediaException
+
+        # The tweet itself doesn't have any media entities. Time to traverse and look for one
+        while tweet.in_reply_to_status_id:
+            self.log.info(f'Looking up parent tweet ID ( {tweet.id} => {tweet.in_reply_to_status_id} )')
+            cache = self.get_tweet(tweet.in_reply_to_status_id)
+            tweet = cache.tweet
+
+            # If this is our own post, that means we've already responded to this thread and need to abort, as all
+            # replies after this point will forcibly include a mention to us.
+            if (tweet.author.id == self.my.id) or (tweet.author.id == SAUCENAOPLS_TWITTER_ID):
+                self.log.info(f"Skipping a comment thread we've already responded to via tweet {tweet.id}")
+                raise TwSauceNoMediaException
+
+            # When someone mentions us to get the sauce of an item, we need to make sure that when others comment
+            # on that reply, we don't take that as them also requesting the sauce to the same item.
+            # This is due to the weird way Twitter's API works. The only best way I know to do this is to look up the
+            # parent tweet ID and see if we're mentioned anywhere in it. If we are, don't reply again.
+            if f'@{self.my.screen_name}' in tweet.full_text:
+                self.log.info(f"Skipping a reply to a bot mention via tweet {tweet.id}")
+                raise TwSauceNoMediaException
+
+            # Any media content?
+            if self.extract_media(tweet):
+                self.log.debug('Media found; breaking traversal')
+                break
+
+            # Nothing yet? Continue until we hit the top of the chain
+
+    def _is_bot_reply(self, tweet) -> bool:
+        """
+        Check and see if this tweet is a reply to a post made by the bots account.
+        We can't support queries for the sauce on our own posts, so we just assume we're responsible enough to supply
+        this info manually.
+        The reason we can't support this is because all replies to our own tweets will have `@botaccount` in their reply
+        text, meaning there's no way to differentiate a direct mention and a normal reply to anything.
+        If we didn't do this, we'd be responding to every single comment trying to provide the sauce of the original post.
+        Returns:
+            bool
+        """
+        if tweet.in_reply_to_status_id:
+            parent = self.get_tweet(tweet.in_reply_to_status_id)
+            return (parent.tweet.author.id == self.my.id) or (parent.tweet.author.id == SAUCENAOPLS_TWITTER_ID)
+
+        return False
+
+    @staticmethod
+    def extract_media(tweet) -> Optional[List]:
+        """
+        Extracts the media attribute from a Tweet
+        Technically, we should only ever need to check for extended_entities since we migrated to using extended tweet
+        lookups. But I'm leaving the old check here just in-case.
+        Args:
+            tweet:  tweepy.models.Status
+
+        Returns:
+            Optional[List[dict]]
         """
         try:
             media = tweet.extended_entities['media']  # type: List[dict]
@@ -51,82 +138,6 @@ class TweetParser:
             try:
                 media = tweet.entities['media']  # type: List[dict]
             except KeyError:
-                self.log.info(f"Tweet {tweet.id} does not have any downloadable media")
-                raise TwSauceNoMediaException
+                return None
 
-        return media
-
-    def _parse_reply(self, tweet) -> List[dict]:
-        """
-        If we were mentioned in a reply, we want to get the sauce to the message we replied to
-        """
-        # First, check and see if this is a reply to a post made by us
-        if tweet.in_reply_to_status_id:
-            parent = self._get_status(tweet.in_reply_to_status_id)
-            if parent.author.id == self.my.id:
-                self.log.info("This is a comment on our own post; ignoring")
-                raise TwSauceNoMediaException
-
-            if parent.author.id == SAUCENAOPLS_TWITTER_ID:
-                self.log.info("The official SauceNaoPls account has already responded to this post; ignoring")
-                raise TwSauceNoMediaException
-
-        while tweet.in_reply_to_status_id:
-            # If this is a post by the SauceNao bot, abort, as it means we've already responded to this thread
-            if tweet.author.id == self.my.id:
-                self.log.info(f"We've already responded to this comment thread; ignoring")
-                raise TwSauceNoMediaException
-
-            if tweet.author.id == SAUCENAOPLS_TWITTER_ID:
-                self.log.info("The official SauceNaoPls account has already responded to this post; ignoring")
-                raise TwSauceNoMediaException
-
-            # Get the parent comment / tweet
-            self.log.info(f"Looking up parent tweet ID ( {tweet.id} => {tweet.in_reply_to_status_id} )")
-            tweet = self._get_status(tweet.in_reply_to_status_id)
-
-            # When someone mentions us to get the sauce of an item, we need to make sure that when others comment
-            # on that reply, we don't take that as them also requesting the sauce to the same item.
-            # This is due to the weird way Twitter's API works. The only sane way to do this is to look up the
-            # parent tweet ID and see if we're mentioned anywhere in it. If we are, don't reply again.
-            if f'@{self.my.screen_name}' in tweet.full_text:
-                self.log.info("This is a reply to a mention, not the original mention; ignoring")
-                raise TwSauceNoMediaException
-
-            # Any media content in this tweet?
-            if 'media' in tweet.entities:
-                self.log.info(f"Media content found in tweet {tweet.id}")
-                break
-
-            if hasattr(tweet, 'extended_entities') and 'media' in tweet.extended_entities:
-                self.log.info(f"Media content found in tweet {tweet.id}")
-                break
-
-        # Now we have a direct tweet to parse!
-        return self._parse_direct(tweet)
-
-    def _get_status(self, tweet_id):
-        """
-        Get the specified tweet
-        Args:
-            tweet_id (int): The twitter tweet ID
-
-        Returns:
-            tweepy.Status
-        """
-        try:
-            self.blocked = False  # Reset the blocked status in-case it's just someone in a chain that has blocked us
-            tweet = api.get_status(tweet_id, tweet_mode='extended')
-        except tweepy.TweepError as error:
-            if error.api_code == 136:
-                self.blocked = True
-                if readonly_api:
-                    self.log.warning(f"User has blocked the main account; falling back to read-only API for media parsing tweet {tweet_id}")
-                    tweet = readonly_api.get_status(tweet_id, tweet_mode='extended')
-                else:
-                    self.log.warning(f"User that submitted tweet {tweet_id} has blocked the bots account, unable to perform lookup")
-                    raise error
-            else:
-                raise error
-
-        return tweet
+        return [m['media_url_https'] for m in media]
